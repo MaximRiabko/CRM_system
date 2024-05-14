bbe0b53bf39c8d1cbb958ef24494d8e47ce0603b
Пользователь <dailos91@mail.ru>
2024-05-14 16:39:38 +0300

    Login styles created

--- CRM_system/CRM_system/urls.py
+++ CRM_system/CRM_system/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("users.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
