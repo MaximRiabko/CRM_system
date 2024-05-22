f16faa3a6ee093391c8d2531d33bc67f16722c82
Пользователь <dailos91@mail.ru>
2024-05-22 17:38:03 +0300

    Style Fix Service

--- CRM_system/services/forms.py
+++ CRM_system/services/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Service
 
 class ServiceCreationForm(ModelForm):
@@ -9,4 +10,10 @@ class ServiceCreationForm(ModelForm):
             'name': 'Название',
             'description': 'описание',
             'price': 'цена'
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'input-text'}),
+            'description': forms.Textarea(attrs={'class': 'textarea_service', 'style': 'resize:none'}),
+            'price': forms.TextInput(attrs={'class': 'input-text'}),
         }
